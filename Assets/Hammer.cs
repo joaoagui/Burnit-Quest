cb429367c1f15122e8cc1c77c0ed939cc5d7e2c1
João Gabriel Guimarães <jotageguimaraes@gmail.com>
2021-04-29 14:41:07 -0400

    2_1 50% complete!

---
+++ Assets/Hammer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+    public float BumpPower;
+    public AudioSource bump;
+    private Rigidbody2D rb;
+    public bool Multiplayer = false;
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.gameObject.CompareTag("Player"))
+        {
+            rb = hitInfo.gameObject.GetComponent<Rigidbody2D>();
+            Health.invincibilityTimer = 0;
+            rb.velocity = new Vector2(BumpPower * -1, 2);
+            bump.Play();
+        }
+
+        if (hitInfo.gameObject.CompareTag("Chay") && Multiplayer == true)
+        {
+            rb = hitInfo.gameObject.GetComponent<Rigidbody2D>();
+            Health.invincibilityTimerP2 = 0;
+            rb.velocity = new Vector2(BumpPower * -1, 2);
+            bump.Play();
+        }
+    }
+}
---
+++ Assets/SecretWall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretWall : MonoBehaviour
+{
+    public GameObject leafParticles;
+    public Transform leafPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+            Instantiate(leafParticles, leafPoint.position, Quaternion.identity);
+        }
+    }
+}
