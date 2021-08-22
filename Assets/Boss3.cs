713442167fb6a84addb208b8b851b232e3c9990e
João Gabriel Guimarães <jotageguimaraes@gmail.com>
2021-08-22 12:58:30 -0400

    BurnitQuest_v0.9.4
    Everything good until lvl 3-5, starting on the Plateaus!

--- Assets/Boss3.cs
+++ Assets/Boss3.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Boss3 : MonoBehaviour
 {
@@ -10,14 +11,29 @@ public class Boss3 : MonoBehaviour
     private IEnumerator coroutine;
 
     private Rigidbody2D rb;
-    public Rigidbody2D Prb;
+    public Transform playerTransform;
     public float speed = 12f;
+    public GameObject bubbleParticles;
+    public GameObject bubblePoint;
+
+
+    public CinemachineTargetGroup CMgroup;
 
     public GameObject coin;
     public GameObject enemyProjectile;
+    public GameObject enemyTentacle;
+    private float randomizer;
+
     public Animator animator;
     public Transform firePoint;
     public float shotNumber; //controls how many times each animation runs
+
+
+    public bool shooting;
+    public bool tentacle;
+    public float tentacleNumber;
+    private float heightmodifier;
+
 
     //audio
     public AudioSource AudioSource;
@@ -25,6 +41,7 @@ public class Boss3 : MonoBehaviour
     public AudioClip bossDamage;
     public AudioClip bossTentacleAttack;
     public AudioClip bossShoot;
+
 
     //die
 
@@ -43,12 +60,41 @@ public class Boss3 : MonoBehaviour
         defaultMaterial = sprite.material;
     }
 
+    private void Update()
+    {
+        if((transform.position.y - playerTransform.position.y) >= 20)
+        {
+            CMgroup.m_Targets[1].weight = 0;
+        }
+
+        else if ((transform.position.y - playerTransform.position.y) < 20)
+        {
+            CMgroup.m_Targets[1].weight = 1;
+        }
+
+        randomizer = Random.Range(0, 10);
+
+        animator.SetBool("Tentacles", tentacle);
+        animator.SetFloat("Shots", shotNumber);
+
+        if(shotNumber < 1)
+        {
+            tentacle = true;
+        }
+
+        else if (shotNumber >= 1)
+        {
+            tentacle = false;
+        }
+    }
+
     public void Shoot()
     {
         if (PauseMenu.paused == false)
         {
             AudioSource.PlayOneShot(bossShoot, 0.8f);
             Instantiate(enemyProjectile, firePoint.position, Quaternion.identity);
+            shotNumber -= 1;
         }
     }
 
@@ -56,18 +102,42 @@ public class Boss3 : MonoBehaviour
     {
         if (PauseMenu.paused == false)
         {
-            AudioSource.PlayOneShot(bossMove, 0.8f);
-            rb.AddForce(new Vector2(0, speed), ForceMode2D.Impulse);
+            if ((transform.position.y - playerTransform.position.y) < 20 && transform.position.y < 660)
+            {
+                AudioSource.PlayOneShot(bossMove, 0.8f);
+                rb.AddForce(new Vector2(0, speed), ForceMode2D.Impulse);
+                Instantiate(bubbleParticles, bubblePoint.transform.position, Quaternion.identity);
+
+            }
         }
     }
 
     public void TentacleAttack()
     {
+
         if (PauseMenu.paused == false)
         {
             AudioSource.PlayOneShot(bossMove, 0.8f);
-            rb.AddForce(new Vector2(0, speed), ForceMode2D.Impulse);
+            heightmodifier = Random.Range(0, 10);
+
+            if (randomizer > 5 )
+            {
+                GameObject newTentacle = Instantiate(enemyTentacle, new Vector2(2.5f, playerTransform.position.y + heightmodifier), Quaternion.Euler(0,0,90));
+                newTentacle.transform.localScale = new Vector2(1, 1);
+            }
+
+            if (randomizer <= 5)
+            {
+                GameObject newTentacle = Instantiate(enemyTentacle, new Vector2(-2.5f, playerTransform.position.y + heightmodifier), Quaternion.Euler(0, 0, 90));
+                newTentacle.transform.localScale = new Vector2(1, -1);
+            }
         }
+    }
+
+    public void TentacleFinished()
+    {
+        tentacle = false;
+        shotNumber = Random.Range(1,2);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -123,7 +193,7 @@ public class Boss3 : MonoBehaviour
         }
 
         Instantiate(BossDie, diePoint.position, Quaternion.identity);
-        StageEnd.transform.position = new Vector2(rb.transform.position.x + 20, StageEnd.transform.position.y);
+        StageEnd.SetActive(true);
         Destroy(gameObject);
         Destroy(LifeBar, 2f);
     }
