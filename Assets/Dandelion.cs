e8c53e48cb81d738b047d9012c7508bb987b68b6
João Gabriel Guimarães <jotageguimaraes@gmail.com>
2021-08-24 10:42:24 -0400

    BurnitQuest_v0.9.5
    Stage 4-1 & 4-2 starting to take shape!

---
+++ Assets/Dandelion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dandelion : MonoBehaviour
+{
+    private Animator animator;
+    private bool get;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && get == false)
+        {
+            get = true;
+            animator.SetBool("get", true);
+        }
+    }
+}
